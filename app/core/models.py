24a923dd9e495ef2ef745917bca1471833bad6b6
solomonT <tseesolo@gmail.com>
2025-04-29 01:46:39 +0100

    Added image handling dependies & configure the statics files & modified the model for user

--- app/core/models.py
+++ app/core/models.py
@@ -2,7 +2,6 @@ from django.db import models
 from django.conf import settings
 from django.core.validators import MinLengthValidator
 from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin, BaseUserManager
-from django.utils.translation import gettext_lazy as _
 
 
 
