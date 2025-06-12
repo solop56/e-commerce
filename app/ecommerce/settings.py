ebed71e586f9387c7fef3b8dc5831f47745b6f3b
solomonT <tseesolo@gmail.com>
2025-06-12 23:36:49 +0100

    Rectified the path fir image

--- app/ecommerce/settings.py
+++ app/ecommerce/settings.py
@@ -159,7 +159,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = '/static/'
-MEDIA_URL = '/static/media/'
+MEDIA_URL = '/media/'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
